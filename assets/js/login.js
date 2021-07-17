4b108c26ccaf7dd9299f62f7933cbf2bc98ccb28
tcy <122632374@qq.com>
2021-07-17 17:49:32 +0800

    完成了登录和注册功能的开发

---
+++ assets/js/login.js
@@ -0,0 +1,64 @@
+$(function() {
+    // 点击注册，跳转到注册
+    $('#link_reg').on('click', function() {
+            $('.login-box').hide()
+            $('.reg-box').show()
+        })
+        //点击去登录，跳转到登录
+    $('#link_login').on('click', function() {
+            $('.login-box').show();
+            $('.reg-box').hide();
+        })
+        //从layui中获取form对象
+    var form = layui.form;
+    var layer = layui.layer;
+    //通过form.verify()函数自定义校验规则
+    form.verify({
+            //自定义密码的校验规则
+            psw: [
+                /^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'
+            ],
+            repsw: function(value) {
+                //拿到密码框的内容，进行比较
+                var psw = $('.reg-box [name=password]').val()
+                if (psw != value) {
+                    return '两次密码不一致';
+                }
+            }
+        })
+        //监听注册表单提交事件
+    $('#form_reg').on('submit', function(e) {
+            e.preventDefault();
+            $.post('http://api-breakingnews-web.itheima.net/api/reguser', {
+                    username: $('#form_reg [name=username]').val(),
+                    password: $('#form_reg [name=password]').val()
+                },
+                function(res) {
+                    if (res.status !== 0) {
+                        return layer.msg(res.message);
+                    }
+                    layer.msg("注册成功，请登录！");
+                    $('#link_login').click();
+                })
+        })
+        //监听登录表单提交事件
+    $('#form_login').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            url: 'http://api-breakingnews-web.itheima.net/api/login',
+            method: 'POST',
+            //快速获取表单数据
+            data: $(this).serialize(),
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg('登录失败')
+                }
+                layer.msg('登陆成功！');
+                //将登录成功得到的token字符串保存到Localstorage中
+                localStorage.setItem('token', res.token);
+
+                location.href = '/index.html';
+            }
+        })
+    })
+})
